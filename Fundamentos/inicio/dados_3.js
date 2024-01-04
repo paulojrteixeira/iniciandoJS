c50a93e87f1ef648d6c3a74ec821d23cf366d4d4
paulojrteixeira <paulojr.teixeira@hotmail.com>
2024-01-04 16:29:14 -0300

    Dados, number, boolaean, string

---
+++ Fundamentos/inicio/dados_3.js
@@ -0,0 +1,20 @@
+let idade = 30;
+console.log(typeof 30);
+console.log(typeof idade);
+//TERMINAL AQUI
+//number
+//numer 
+
+let salario = 4509.12;
+console.log(typeof salario);
+//TERMINAL AQUI
+//number
+
+let estaChovendo = true; // or false
+console.log(typeof estaChovendo);
+//TERMINAL AQUI
+//boolean 
+
+console.log(typeof "teste");
+//TERMINAL AQUI
+//string 
