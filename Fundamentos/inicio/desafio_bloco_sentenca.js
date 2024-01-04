57d3f799d47a12158fe45f2c24d8629c662449dc
paulojrteixeira <paulojr.teixeira@hotmail.com>
2024-01-04 14:56:43 -0300

    desafio bloco e sentença

---
+++ Fundamentos/inicio/desafio_bloco_sentenca.js
@@ -0,0 +1,22 @@
+/*
+    EXIBIR NO CONSOLE OS NUMEROS DE 1 A 10
+    SEPARAR EM BLOCOS DIFERENTES IMPARES E PARES
+
+*/
+
+
+{
+    console.log(1);
+    console.log(3);
+    console.log(5);
+    console.log(7);
+    console.log(9);  }
+
+
+ {
+    console.log(2);
+    console.log(4);
+    console.log(6);
+    console.log(8);
+    console.log(10);
+}   
