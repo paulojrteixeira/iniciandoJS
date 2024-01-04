0ac5ff205f6d1bd26e78deb735cbdbbeb9955833
paulojrteixeira <paulojr.teixeira@hotmail.com>
2024-01-04 16:14:11 -0300

    variavel let

---
+++ Fundamentos/inicio/dados_2.js
@@ -0,0 +1,19 @@
+let preco = 19.90;
+let desconto = 0.4;
+let precoComDesconto = preco * (1-desconto);
+
+console.log(precoComDesconto)
+
+let nome = "caderno";
+let categoria = "papelaria";
+
+console.log("Produto:" + nome 
+    + ",Categoria:" + categoria 
+    + ",Preço:" + preco
+    + ",Desconto:" + precoComDesconto); 
+
+    // CONCATENAR = JUNTAR UM TEXTO NO OUTRO [  +""+  ] SE DER UM ESPAÇO LÁ NO MEIO DAS ASPAS,
+    // VAI SEPARAR O TEXTO NO CONSOLE //
+
+
+    // STRING = TEXTO --> SEQUÊNCIA DE LETRAS E SÍMBOLOS //
