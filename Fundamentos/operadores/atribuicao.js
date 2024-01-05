ea846d9acb0aa568b4028eb0b68e89c72796c5ee
paulojrteixeira <paulojr.teixeira@hotmail.com>
2024-01-05 16:58:36 -0300

    atribuições

---
+++ Fundamentos/operadores/atribuicao.js
@@ -0,0 +1,16 @@
+let x = 10;
+x = x + 6;
+x = x - 1 ;
+console.log(x)
+
+let y = 20;
+y += 10;    // ATRIBUIÇÃO ADITIVA (+=)
+y -= 2;     // ATRIBUIÇÃO SUBTRATIVA (-=)
+y *= 2;     // ATRIBUIÇÃO MULTIPLICATIVA (*=)
+y /= 4;     // ATRIBUIÇÃO DIVISIVA (/=)
+y %= 3;     // ATRIBUIÇÃO COM MODULO (%=)
+console.log(y)
+
+let w = "texto";
+w += ", escreva aqui!"
+console.log(w)
