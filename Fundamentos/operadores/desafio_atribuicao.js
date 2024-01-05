d4601a8d40ad4395009127efe0193aac8df73d37
paulojrteixeira <paulojr.teixeira@hotmail.com>
2024-01-05 17:39:19 -0300

    desafio de atriuições - Lista de Aprovados

---
+++ Fundamentos/operadores/desafio_atribuicao.js
@@ -0,0 +1,20 @@
+/*  /// MONTAR UMA LISTA DE APROVADOS UTILIZANDO OS OPERADORES DA AULA: ///
+
+     LISTA DE APROVADOS
+    ===================
+    
+    1 - Ana Silva
+    2 - Pedro Albuquerque
+    3 - Guilherme Pereira
+    4 - Rebeca França
+*/
+
+let texto = "Lista de Aprovados\n" 
+texto += "=============\n\n "
+texto += "Ana Silva;\n "
+texto += "Pedro Albuquerque;\n "
+texto += "Guilherme Pereira;\n "
+texto += "Rebeca França:"
+console.log(texto);
+
+/// OKOKOK ///
