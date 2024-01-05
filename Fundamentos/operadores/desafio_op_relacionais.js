30ef4768510194b78194824f031f8f1635861cf0
paulojrteixeira <paulojr.teixeira@hotmail.com>
2024-01-05 16:17:36 -0300

    desafio e correção op. relacionais

---
+++ Fundamentos/operadores/desafio_op_relacionais.js
@@ -0,0 +1,38 @@
+/// ENCONTRAR 5 EXPRESSOES QUE O RESULTADO SEJA TRUE
+
+let a = 1;
+let b = 2;
+
+console.log(a < b);
+console.log(a <= b);
+console.log(a != b);
+console.log(a !== b);
+console.log(a + a === b);
+
+/// ENCONTRAR 5 EXPRESSOES QUE O RESULTADO SEJA FALSE
+
+console.log(a > b)
+console.log(a >= b)
+console.log(a =! b)
+console.log(a == b)
+console.log(a + a === b)
+
+/* /// CORREÇÃO ///
+
+    TRUE:
+    let a = 1;
+    let b = 2;
+
+    console.log(a < b);
+    console.log(a <= b);
+    console.log(a != b);
+    console.log(a !== b);
+    console.log(a + a === b);
+
+    FALSE:
+    console.log(a > b)
+    console.log(a >= b)
+    console.log(a == b)
+    console.log(a === b)
+    console.log(a + a !== b)
+ */
