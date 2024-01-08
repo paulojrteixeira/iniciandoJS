75f45f597a087cb2e26bf7e1782f87d3c571bd83
paulojrteixeira <paulojr.teixeira@hotmail.com>
2024-01-08 19:55:01 -0300

    unarios 3

---
+++ Fundamentos/operadores/unarios_3.js
@@ -0,0 +1,12 @@
+console.log(!true);
+console.log(!!false);
+
+console.log(!"texto");  /// !! = DUPLA NEGAÇÃO  ///
+console.log(!!"");  /// STRING VAZIA SEMPRE VAI DAR FALSO   ///
+
+console.log(!!1);
+console.log(!!0);
+console.log(!!-1);
+console.log(!!-0);
+
+/// TODOS OS NÚMEROS COM EXCEÇÃO DO ZERO SERÃO CONVERTIDOS PARA TRUE (VERDADEIRO)  ///
