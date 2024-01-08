9481d14d08494d954f19dcdb837b5ec4ac77f07b
paulojrteixeira <paulojr.teixeira@hotmail.com>
2024-01-08 19:15:09 -0300

    unarios 1

---
+++ Fundamentos/operadores/unarios_1.js
@@ -0,0 +1,21 @@
+let a = 1;
+
+a = a + 1;
+a += 1;
+a++;
+++a;
+
+a--;
+--a;
+
+console.log(a);
+
+/* INFIX - (OPERADOR NO MEIO DOS OPERANDOS)
+    EXEMPLO: a + b
+   
+   POSTFIX - (OPERADOR DEPOIS DO OPERANDO)
+    EXEMPLO: a++
+
+   PREFIX - (OPERADOR ANTES DO OPERANDO) 
+    EXEMPLO ++a
+ */
