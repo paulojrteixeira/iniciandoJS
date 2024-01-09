1e88f65456251d1101b53c1cffc34f83c710f9a0
paulojrteixeira <paulojr.teixeira@hotmail.com>
2024-01-09 18:31:05 -0300

    Desafio corrigido IF notas ABCDF

---
+++ Fundamentos/controle/desafio_if.js
@@ -0,0 +1,21 @@
+const nota = 7
+
+if(nota <= 10 && nota >= 9) {
+console.log("A")
+}
+
+if(nota < 9 && nota >= 7) {
+console.log("B")
+}
+
+if(nota < 7 && nota >= 5) {
+console.log("C")
+}
+
+if(nota < 5 && nota >= 4.5) {
+console.log("D")
+}
+
+if(nota < 4.5) {
+console.log("F")
+}
