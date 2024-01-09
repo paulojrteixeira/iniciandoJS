465ad2b738c0a04ff82b1d204cdaf016ce50100b
paulojrteixeira <paulojr.teixeira@hotmail.com>
2024-01-09 20:16:32 -0300

    IF/ELSE 5

--- Fundamentos/controle/if_else_5.js
+++ Fundamentos/controle/if_else_5.js
@@ -3,13 +3,16 @@ let saudacao;
 
 if (hora < 12) {
     saudacao = 'Bom Dia';
-}
-else if (hora < 18) {
-    saudacao = 'Boa Tarde';
-} else if (hora < 22) {
-    saudacao = 'Boa Noite';
 } else {
-    saudacao = 'Muito tarde'
+    if (hora < 18) {
+        saudacao = 'Boa Tarde';
+    } else {
+        if (hora < 22) {
+            saudacao = 'Boa Noite';
+        } else {
+            saudacao = 'Muito tarde'
+        }
+    }
 }
 
 console.log(saudacao);
