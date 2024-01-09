54b929d8056307381a2306c02476da1a7e4a660d
paulojrteixeira <paulojr.teixeira@hotmail.com>
2024-01-09 19:24:13 -0300

    IF/ELSE 1

---
+++ Fundamentos/controle/if_else.js
@@ -0,0 +1,12 @@
+const verdade = true
+const mentira = false
+
+if(verdade)
+    console.log('Exec');
+else
+    console.log('Nao exec');
+
+if(mentira)
+    console.log('Nao exec');
+else 
+    console.log('Exec');
