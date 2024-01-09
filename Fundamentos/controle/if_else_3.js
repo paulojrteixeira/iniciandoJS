2ee192ed10e28bf9f1ad14bd7642467ac6949c29
paulojrteixeira <paulojr.teixeira@hotmail.com>
2024-01-09 19:52:12 -0300

    IF/ELSE 3

---
+++ Fundamentos/controle/if_else_3.js
@@ -0,0 +1,14 @@
+const hora = 5
+let saudacao;
+
+if(hora < 12){
+    saudacao = 'Bom Dia';
+} else {
+    if(hora < 18) {
+    saudacao = 'Boa Tarde';
+    } else { 
+        saudacao = 'Boa Noite';
+    }
+}
+    
+console.log(saudacao);
