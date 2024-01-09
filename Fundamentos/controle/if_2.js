64a80963f9ce19cc14187fcfc31161fb69693b29
paulojrteixeira <paulojr.teixeira@hotmail.com>
2024-01-09 17:27:27 -0300

    IF 2

---
+++ Fundamentos/controle/if_2.js
@@ -0,0 +1,10 @@
+const nota = 10;
+const bomComportamento = true;
+
+if(nota>= 8 && bomComportamento)
+    console.log("Parabéns!!");
+
+if(nota < 6 || !bomComportamento)
+    console.log("Bom");
+
+console.log("FIM!");
