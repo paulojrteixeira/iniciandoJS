a9965188b1ad7db572807972d86a06a4870322d9
paulojrteixeira <paulojr.teixeira@hotmail.com>
2024-01-09 18:04:16 -0300

    Desafio e Corrreção IF 4

---
+++ Fundamentos/controle/if_4.js
@@ -0,0 +1,24 @@
+const nota = 8.99;
+
+if (nota>= 9) {
+    console.log("Com Honra!!");
+    console.log("Parabéns!!");
+}
+console.log("FIM!");
+
+
+/*      RESPOSTA DO DESAFIO: ///
+EXEMPLO DO DESAFIO:
+
+const nota = 8;
+
+if (nota>= 9); {
+    console.log("Com Honra!!");
+    console.log("Parabéns!!");
+}
+console.log("FIM!");
+
+/// O BLOCO IF(NOTA), ENCONTROU UMA SENTENÇA DE CODIGO VAZIA ( ; )
+/// MESMO A NOTA ESTANDO ABAIXO, VAI MOSTRAR OS PARABÉNS
+/// EVITAR POR ( ; ) EM ESTRUTURAS DE CONTROLE!!!!  UMA EXCEÇÃO SERÁ DITA NA PROXIMA AULA. 
+*/
