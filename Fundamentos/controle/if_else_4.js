255c7263a4fe9b6d16b1b0db5382c780be43ae6f
paulojrteixeira <paulojr.teixeira@hotmail.com>
2024-01-09 20:07:35 -0300

    IF/ELSE 4

---
+++ Fundamentos/controle/if_else_4.js
@@ -0,0 +1,15 @@
+const hora = 23
+let saudacao;
+
+if (hora < 12) {
+    saudacao = 'Bom Dia';
+}
+else if (hora < 18) {
+    saudacao = 'Boa Tarde';
+} else if (hora < 22) {
+    saudacao = 'Boa Noite';
+} else {
+    saudacao = 'Boa tarde'
+}
+
+console.log(saudacao);
