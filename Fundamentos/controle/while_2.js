3e90571c13a7e210660270a58f9db6f5d0ffa3fc
paulojrteixeira <paulojr.teixeira@hotmail.com>
2024-01-10 15:26:59 -0300

    WHILE 2

---
+++ Fundamentos/controle/while_2.js
@@ -0,0 +1,9 @@
+let quantidade = 0
+
+/// QUANTIDADF INDETERMINADA DE REPETIÇÕES  ///
+while(Math.random () < 0.9) {
+    console.log('Tente outra vez')
+    quantidade++;
+}
+
+console.log('Qtde: ' + quantidade)
