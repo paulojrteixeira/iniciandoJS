005988b9f6897dfd4e57e5f1b6e81bee47244e1e
paulojrteixeira <paulojr.teixeira@hotmail.com>
2024-01-11 16:39:58 -0300

    Função 2

---
+++ Fundamentos/funcao/funcao_2.js
@@ -0,0 +1,22 @@
+const d1 = 24;
+const m1 = 9;
+const a1 = 2023;
+
+const d2 = 12
+const m2 = 12
+const a2 = 2012
+
+function exibirData2() {
+    console.log(`${d2}/${m2}/${a2}`);
+}
+function exibirData(d, m, a) {
+    console.log(`${d}/ ${m}/ ${a}`) /// DEIXEI COM ESPAÇO PARA DIFERENCIAR O RESULTADO NO TERMINAL
+}
+
+exibirData(d1, m1, a1);
+exibirData(d1, m1, a1);
+exibirData(d1, m1, a1);
+
+exibirData(d2, m2, a2);
+exibirData(d2, m2, a2);
+exibirData(d2, m2, a2);
