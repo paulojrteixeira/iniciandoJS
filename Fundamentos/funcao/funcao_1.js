7672a8caca681a030cabddd1888f47d160db1432
paulojrteixeira <paulojr.teixeira@hotmail.com>
2024-01-11 15:40:27 -0300

    Função 1

---
+++ Fundamentos/funcao/funcao_1.js
@@ -0,0 +1,23 @@
+const d1 = 24;
+const m1 = 9;
+const a1 = 2023;
+
+function exibirData1() {
+    console.log(`${d1}/${m1}/${a1}`);
+}
+
+const d2 = 12
+const m2 = 12
+const a2 = 2012
+
+function exibirData2() {
+    console.log(`${d2}/${m2}/${a2}`);
+}
+
+exibirData1();
+exibirData1();
+exibirData1();
+
+exibirData2();
+exibirData2();
+exibirData2();
