5227b23687381b95bda9c5b924805a0ac6390668
paulojrteixeira <paulojr.teixeira@hotmail.com>
2024-01-11 17:00:01 -0300

    Desafio função corrigido

---
+++ Fundamentos/funcao/desafio_funcao_1.js
@@ -0,0 +1,17 @@
+/// RECEBER 2 PARAMETROS E SOMAR 5 = 10, 6 + 3
+
+function somar(n1 , n2) {
+    console.log(n1 + n2);
+}
+somar(5,10);
+somar(6, 3);
+
+// RECEBER 2 PARAMETROS E SUBTRAIR 10 - 8, 90 - 40
+
+function subtrair(n3 , n4){
+    console.log(n3 - n4);
+}
+subtrair(10, 8);
+subtrair(90, 40);
+
+///     OKOK!!      ////
