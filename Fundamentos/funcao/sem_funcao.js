d0dbeb821dd9ee60d31ab963139aec356da28174
paulojrteixeira <paulojr.teixeira@hotmail.com>
2024-01-11 14:59:32 -0300

    Função

---
+++ Fundamentos/funcao/sem_funcao.js
@@ -0,0 +1,15 @@
+const d1 = 24;
+const m1 = 9;
+const a1 = 2023;
+
+console.log(`Dia ${d1}`);
+console.log(`Mês ${m1}`);
+console.log(`Ano ${a1}`);
+
+const d2 = 12
+const m2 = 12
+const a2 = 2012
+
+console.log(`Dia ${d2}`);
+console.log(`Mês ${m2}`);
+console.log(`Ano ${a2}`);
