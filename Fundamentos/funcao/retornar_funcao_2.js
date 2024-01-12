7fd3b7e8086dd6ddad9fc23384dbae64f1f6818c
paulojrteixeira <paulojr.teixeira@hotmail.com>
2024-01-12 20:33:46 -0300

    retornar função 2

---
+++ Fundamentos/funcao/retornar_funcao_2.js
@@ -0,0 +1,9 @@
+function retornarUmaFuncao() {
+    return function (){
+        return 'Boa Tarde!'
+    }
+}
+
+console.log(retornarUmaFuncao);
+console.log(retornarUmaFuncao());
+console.log(retornarUmaFuncao()());
