348b0b118eaa9cc194c9cf830c93ca7a3f85487b
paulojrteixeira <paulojr.teixeira@hotmail.com>
2024-01-15 17:29:02 -0300

    Basico Objetos 1

---
+++ Fundamentos/objetos/basico_1.js
@@ -0,0 +1,13 @@
+const numeros = [1, 2, 3];
+const produto = {
+    nome: 'Caneta',
+    preco: 0.99
+}
+
+console.log(numeros[0]);
+console.log(produto['nome']);
+console.log(produto['preco']);
+
+console.log(produto.nome);
+console.log(produto.preco);
+console.log(typeof produto);
