eb21b3ef1383045ea4a264004411932f311e6fbe
paulojrteixeira <paulojr.teixeira@hotmail.com>
2024-01-15 15:37:18 -0300

    Basico Array 3

---
+++ Fundamentos/array/basico_3.js
@@ -0,0 +1,10 @@
+console.log(typeof console);        /// OBJECT
+console.log(typeof console.log);    /// FUNCTION
+
+const numeros = [1, 2, 3];
+numeros.push(9);
+numeros.push(6);
+
+console.log(numeros);
+console.log(typeof numeros);        /// OBJECT
+console.log(numeros.length);
