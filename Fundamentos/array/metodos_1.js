07e9dd670517f5431f8876507658bc50c5d9f2af
paulojrteixeira <paulojr.teixeira@hotmail.com>
2024-01-15 16:22:23 -0300

    Metodos Array 1

---
+++ Fundamentos/array/metodos_1.js
@@ -0,0 +1,9 @@
+const numeros = [1, 2, 3, 4, 5];
+
+numeros.push(6);
+numeros[0] = 999;
+
+console.log(numeros);
+console.log(numeros.join (' # '));
+console.log(numeros.includes(500));
+console.log(numeros.indexOf(4));
