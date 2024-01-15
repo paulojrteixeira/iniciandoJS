39418135be6551c5923a22299f44090d64f8a41e
paulojrteixeira <paulojr.teixeira@hotmail.com>
2024-01-15 16:13:14 -0300

    Desafio FOR Array CORRIGIDO

---
+++ Fundamentos/array/desafio_for_array.js
@@ -0,0 +1,12 @@
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 8; i >= 0; i -= 2) {
+    console.log(numeros[i]);
+}
+
+///     OKOK!!  ///
+/// CORREÇÃO    ///
+
+// for(let i = numeros.length - 2; i >= 0; i -= 2) {
+// console.log(numeros[i]);
+// }
